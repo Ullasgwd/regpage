3f70559b92558c5f2448419ff44520ddf947a3f2
Ullas Gowda S <ullas.s@utthunga.in>
2021-08-27 20:31:38 +0530

    registration page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form'
+import Particles from 'react-particles-js';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form/>
+      <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 200
+	        },
+	        "size": {
+	            "value": 5
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        }
+	    }
+	}} 
+  
+  height="150vh"
+  style={{
+   // background: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHfL0U8XFVOImoWxHfl8qVe30yLnSQiZwqDQ&usqp=CAU")`,
+    //backgroundRepeat:'no-repeat' ,
+    //backgroundSize:'cover',
+  
+    
+  }}
+  />
+
     </div>
+
   );
 }
 
